20867ea17531f0e9b93d76a47ace79dd204c0b66
tizba <baptou.audeon@gmail.com>
2023-01-10 11:08:10 +0100

    1er save

---
+++ APQuizz.py
@@ -0,0 +1,17 @@
+# Quiz QCM
+# 4 à 16 ans
+# entièrement paramétrable sans nécessiter connaissances en info 
+# doc utilisateur permettant aux animateurs  comprendre créer modifier eux-mêmes leurs quiz
+# Il n'y a pas de limite pour le nombre de questions.
+# Le nombre d'essai sera paramétrable.
+# Les questions peuvent avoir plusieurs réponses possibles sans limite mais le joueur ne peut saisir qu'une seule réponse par question.
+# Les écrans du jeu (le jeu par lui-même et l'affichage des résultats) garderont un aspect semblable aux écrans du jeu de géographie. (Annexe 2)
+# Les points attribués à chaque question seront paramétrables.
+# Lors de l'écriture de la solution, il faut rendre le jeu complètement paramétrable (ce qui n'est pas le cas dans le jeu ci dessous.) c'est-à-dire que la feuille de code développé doit s'adapter aux changements sans modification de code. Ainsi, le thème, les questions, les réponses, le nombre d'essai et les points seront donc définis dans un fichier séparé du code pour s'adapter au jeu (fichier .JSON par exemple)
+# Lors de la phase de recette et lors de la démo, le jeu doit être capable de bien réagir aux données du recetteur. (Annexe 3)
+# Le code sera documenté pour le comprendre du mieux possible.
+# l faudra concevoir une notice utilisateur indiquant le mode opératoire pour adapter le jeu aux besoins de l'utilisateur.
+
+
+
+
